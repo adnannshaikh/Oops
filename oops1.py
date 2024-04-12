0be5b2f2a5c9fd275dd04d05c33fefd8e1592e26
Adnan <sikadnan2@gmail.com>
2024-04-12 08:46:42 +0530

    Modified the oops1 file

--- oops1.py
+++ oops1.py
@@ -1,4 +1,4 @@
-# oop
+ # oop
 print(type(None))
 print(type(True))
 print(type(5))
@@ -8,3 +8,8 @@ print(type([]))
 print(type(()))
 print(type({}))
 print(type({1}))
+
+# creating a class
+
+class BigObject: #camel cases and first alphabet to be capital so that the programmers know that this is a class
+    pass
